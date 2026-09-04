eb9f760caadce1d65dccf17dd51baa395a39aad7
Hitesh-77 <hitesh2182006@gmail.com>
2026-09-04 21:05:19 +0530

    saving fiels

--- app.py
+++ app.py
@@ -1,74 +1,50 @@
-import streamlit as st
+import gradio as gr
 
-st.set_page_config(
-    page_title="AI Toxic Comment Detection System",
-    page_icon="🛡️",
-    layout="centered"
-)
+with gr.Blocks(
+    title = "AI YouTube Toxic Comment Detection System"
+    ) as demo:
 
-with open("style.css", "r", encoding="utf-8") as f:
-    st.markdown(
-        f"<style>{f.read()}</style>",
-        unsafe_allow_html=True
-    )
+    with gr.Column():
+        gr.Markdown(
+            """
+            # 🛡️ AI YouTube Toxic Comment Detection System
 
-# header
-st.markdown("""
-    <div class = "main-title">
-        🛡️ AI YouTube Toxic Comment Detection System
-    </div>
-""", unsafe_allow_html=True)
+            Paste a YouTube video URL.
 
-# intro
-st.markdown("""
-    <div class = "intro">
-        Paste a YouTube video URL.
-    </div>
-""", unsafe_allow_html=True)
+            The system will:
+            
+            ✅ Fetch comments
 
-st.markdown("""
-    <div class="intro" style="margin-top: 8px;">
-        The system will:
-    </div>
+            ✅ Analyze every comment
 
-    <div class="checklist">
-        <div><span class="check">✅</span> Fetch comments</div>
-        <div><span class="check">✅</span> Analyze every comment</div>
-        <div><span class="check">✅</span> Show dashboard statistics</div>
-        <div><span class="check">✅</span> Display all comments</div>
-        <div><span class="check">✅</span> Click any comment to view detailed ML analysis</div>
-    </div>
-""", unsafe_allow_html=True)
+            ✅ Show dashboard statistics
 
-st.markdown(
-    """
-    <div class="youtube-url-label">
-        YouTube Video URL
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+            ✅ Display all comments
 
-video_url = st.text_input(
-    label="YouTube Video URL",
-    placeholder="https://www.youtube.com/watch?v=...",
-    label_visibility="collapsed"
-)
+            ✅ Click any comment to view detailed ML analysis
+            """
+        )
 
-st.markdown("""
-    <div class="comments-slider-label">
-        Maximum Comments
-    </div>
-""", unsafe_allow_html=True)
+        youtube_url = gr.Textbox(
+            label="YouTube Video URL",
+            placeholder="Paste a YouTube video URL here..."
+        )
 
-max_comments = st.slider(
-    label = "Maximum Comments",
-    label_visibility = "collapsed",
-    value = 100,
-    min_value = 10,
-    max_value = 500,
-    step = 10
-)
+        max_comments = gr.Slider(
+            minimum=10,
+            maximum=500,
+            step=10,
+            value=100,
+            label="Maximum Comments",
+            interactive=True
+        )
 
-if st.button("Fetch & Analyze Comments",use_container_width=True):
-    st.write("Button clicked")
+
+if __name__ == "__main__":
+    demo.launch(
+        theme = gr.themes.Soft(
+                primary_hue="blue",
+                secondary_hue="slate"
+            ),
+        css_paths = "style.css"
+        )
